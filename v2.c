80f29127ad11c50d0ea87ecb330dc694af3c7822
AsierCL <asiercl.05@gmail.com>
2025-03-21 13:25:03 +0100

    V2 sin saber si funciona, e makefile original

---
+++ v2.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdalign.h>
+#include <immintrin.h>
+#include <pmmintrin.h>
+#include <string.h>
+#include <omp.h>
+#include <math.h>
+
+
+#include "./counter.h"
+
+
+void rellenarMatriz(float **a, float *b, float *x, int n, int i) {
+    float suma_fila = 0.0f;
+    // Inicializa cada elemento de la fila con (float)rand()/RAND_MAX
+    for (int j = 0; j < n; j++) {
+        a[i][j] = (float)rand() / RAND_MAX;
+        suma_fila += a[i][j];
+    }
+    // 3. Al elemento de la diagonal se le suma la suma total de la fila
+    a[i][i] += suma_fila;
+
+    // Inicializa el vector b aleatoriamente
+    b[i] = (float)rand() / RAND_MAX;
+    // Inicializa el vector x con ceros
+    x[i] = 0.0f;
+}
+
+void imprimirMatriz(float **a, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%f ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    if(argc != 3) {
+        printf("Usage: %s <n> <c>\n", argv[0]);
+        return 1;
+    }
+
+    int n = atoi(argv[1]);
+    int c = atoi(argv[2]);
+    srand(n);
+
+    float **a = _mm_malloc(n * sizeof(float *), 64);
+    float *b = _mm_malloc(n * sizeof(float), 64);
+    float *x = _mm_malloc(n * sizeof(float), 64);
+    float tol = 10e-8;
+    int max_iter = 20000;
+    float *x_new = _mm_malloc(n * sizeof(float), 64);
+    
+    float norm2 = 0;
+
+    //imprimirMatriz(a, n);
+
+    for (int iter = 0; iter < max_iter; iter++) {
+        norm2 = 0;
+        for (int i = 0; i < n; i++) {
+            a[i] = _mm_malloc(n * sizeof(float), 64);
+            rellenarMatriz(a, b, x, n, i);
+            float sigma = 0;
+            for (int j = 0; j < n; j++) {
+                if(i!=j){
+                    sigma += a[i][j] * x[j];
+                }
+            }
+            x_new[i] = (b[i] - sigma) / a[i][i];
+            norm2 += pow(x_new[i] - x[i], 2);
+        }
+
+        //x = x_new;
+        memcpy(x, x_new, n * sizeof(float));
+        if(sqrt(norm2) < tol) {
+            printf("Tolerancia alcanzada en la iteración %d\n", iter);
+            printf("Norma2: %e\n", norm2);
+            return 0;
+        }
+    }
+    
+    printf("Norma2: %e\n", norm2);
+    return 0;
+}
